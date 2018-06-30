d96990c4e61f998478b1f39651b489c5a6026304
Denis Duev <denis_duev@abv.bg>
2018-06-30 12:26:42 +0300

    set cookie on response for auth

--- javascript/authenticate.js
+++ javascript/authenticate.js
@@ -6,27 +6,30 @@ module.exports.authenticate = function (req, res) {
 
     //console.log(username);
     console.log(req.body);
-    connection.query('SELECT * FROM users WHERE username = ? AND password = ?',[username, password], function (error, results, fields) {
-        if(error) {
+    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+        if (error) {
             res.json({
-                status : false,
-                massage : 'There are some error with query!'
+                status: false,
+                massage: 'There are some error with query!'
             })
-        }else {
-            if(results.length > 0) {
+        } else {
+            if (results.length > 0) {
+                res.setHeader('Set-Cookie', "username=" + username);
+                res.setHeader('Set-Cookie', "password=" + password);
                 //console.log(results);
                 res.write(JSON.stringify({
-                    status : true,
-                    massage : 'Successfully authenticated!'}));
+                    status: true,
+                    massage: 'Successfully authenticated!'
+                }));
                 res.end();
-            }else {
+            } else {
                 console.log(results);
                 res.json(JSON.stringify({
-                    status : false,
-                    massage : 'User does not exits!'
+                    status: false,
+                    massage: 'User does not exits!'
                 }));
                 res.end();
             }
         }
     });
-}
+};
