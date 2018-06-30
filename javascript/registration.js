2b1c1b1b42f78f440c3a87c4c7126480ec2ab9c1
Mihaela Chakova <40439040+mmishel@users.noreply.github.com>
2018-06-30 20:49:04 +0300

    Add files via upload

--- javascript/registration.js
+++ javascript/registration.js
@@ -7,22 +7,41 @@ module.exports.register = function (req, res) {
     var sql = 'INSERT INTO `users` (`username`, `password`, `created_at`,`updated_at`) VALUES (?, ?, ?, ?)';
     var today = new Date();
 
-    //console.log(connection);
-    connection.query(sql, [req.body.username, req.body.password, today, today], function (error, results, fields) {
+    var username = req.body.username;
+
+    connection.query('SELECT * FROM users WHERE username = ?', [username], function (error, results, fields) {
         if (error) {
-            console.log(error);
-            res.write(JSON.stringify({
+            res.json({
                 status: false,
                 massage: 'There are some error with query!'
-            }));
-            res.end();
+            })
+        }
+        if(!results.length) {
+            //console.log(connection);
+            connection.query(sql, [username, req.body.password, today, today], function (error, results, fields) {
+                if (error) {
+                    console.log(error);
+                    res.write(JSON.stringify({
+                        status: false,
+                        massage: 'There are some error with query!'
+                    }));
+                    res.end();
+                } else {
+                    res.write(JSON.stringify({
+                        status: true,
+                        data: results,
+                        massage: 'User registered successfully!'
+                    }));
+                    res.end();
+                }
+            });
         } else {
-            res.write(JSON.stringify({
-                status: true,
-                data: results,
-                massage: 'User registered successfully!'
+            console.log(results);
+                res.json(JSON.stringify({
+                    status: false,
+                    massage: 'User already exists!'
             }));
             res.end();
         }
     });
-}
+};
