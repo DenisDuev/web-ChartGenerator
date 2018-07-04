d6a4da9794ded661a539b3f6c54934d07ea3412c
Denis Duev <denis_duev@abv.bg>
2018-07-04 21:52:44 +0300

    add readme and sql script

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # web-ChartGenerator
-An university project for web subject.
+An university project for Web Technologies, X edition, 2018.
+Sofia University, Faculty of Mathematics and Informatics
 
 # Task:
 ## Генератор на диаграми по зададени файлове 
 (например: https://support.office.com/bg-bg/article/Налични-типове-диаграми-10b5a769-100d-4e41-9b0f-20df0544a683#__toc365991018) - ъплоудват се файлове, задават се параметри (в нещо като wizard), които генерират една или повече заявки, дава се възможност да се редактира заявката и след това чертае избран тип диаграма (например среден успех на студенти от първите 5 домашни по www)
+
+#How to setup
+- start mysql database
+- at the beginning of *server.js* edit db settings
+- start the applicatiom using node js (either your or node.exe file) -> *node server.js*
+###setup db:
+- you can import **mydb.sql** file or 
+- stop server, make GET request to /createuserstable, start server
+
+Now you only need to register in the application or use admin/admin
---
+++ mydb.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2018 at 08:47 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'denis', 'denis', '2018-06-30 10:55:00', '2018-06-30 10:55:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
